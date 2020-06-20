6360b88bb39aa2eb435f0d8e46f4ab456c0ca2c2
Rahul Gore <rahul.ravi.gore@gmail.com>
2020-06-20 16:11:39 +0530

    Added feature - filtering recipes by tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,9 +43,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = serializers.RecipeSerializer
 
+    def _params_to_ids(self, qs):
+        """Convert a queryset to list of IDS"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Filter the query set by the authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ids(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ids(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
